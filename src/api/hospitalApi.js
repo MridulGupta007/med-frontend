011192272ab6bcfd9e0bc98c2855f3b502eb5d3c
MridulGupta007 <mridulgupta2003official@gmail.com>
2025-03-20 12:13:32 +0530

    route changed

--- src/api/hospitalApi.js
+++ src/api/hospitalApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/hospitals";
+const API_URL = "https://med-backend-7tg1.onrender.com/api/hospitals";
 
 export const searchHospitals = async (specialization, lat, long, radius, token) => {
   return axios.get(`${API_URL}/search`, {
